7a56f51ea21fa952b2ec72629876a9b5574506c4
Birrus09 <simone.verganj@gmail.com>
2026-03-08 22:13:42 +0100

    Update battle.h

--- Tamer/battle.h
+++ Tamer/battle.h
@@ -5,15 +5,29 @@ struct team{
 };
 
 
+struct Puppa{
+    Move mossa;
+    creature_instance target;
+};
+
 
 void apply_move(Puppa values){
+    if(values.mossa.types[0] == 1){
+
+    }
+    if(values.mossa.types[1] == 1){
+    }
+    if(values.mossa.types[2] == 1){
+    }
+    if(values.mossa.types[3] == 1){
+    }
 
 }
 
 
 
 
-int battle(team team1, team team2){
+int battle(team team1, team team2, team total){
     int outcome = 0;
     int currentturn = 0;
     unsigned int BattleTimer = 0;
@@ -48,7 +62,7 @@ int battle(team team1, team team2){
         for (int i = 0; i<team1.members.size(); i++){
             if (team1.members[i].timer == 0){
                 currentturn = i+1;
-                apply_move(team1.members[i].taketurn());
+                apply_move(team1.members[i].taketurn(total));
             }
         }
     
