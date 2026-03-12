aa2147fdd9ab51c99906a3fb35e4fb9249783f50
Birrus09 <simone.verganj@gmail.com>
2026-03-12 21:48:14 +0100

    Update battle.h

--- Tamer/battle.h
+++ Tamer/battle.h
@@ -7,21 +7,33 @@ struct team{
 
 struct Puppa{
     Move mossa;
-    creature_instance target;
+    creature_instance &target;
+    creature_instance &applier;
 };
 
-
 void apply_move(Puppa values){
-    if(values.mossa.types[0] == 1){
+    if(values.mossa.types[0]){
+        if(values.mossa.types[3]){
+            values.target.hp += values.mossa.power * values.applier.stats[0] / 100;
+        }
+        else{
+            values.target.hp -= values.mossa.power * values.applier.stats[0] / values.target.stats[1];
+        }
+    }
 
-    }
-    if(values.mossa.types[1] == 1){
-    }
-    if(values.mossa.types[2] == 1){
-    }
-    if(values.mossa.types[3] == 1){
+    if(values.mossa.types[1]){
+        if(values.mossa.types[3]){
+            values.target.hp += values.mossa.power * values.applier.stats[2] / 100;
+        }
+        else{
+            values.target.hp -= values.mossa.power * values.applier.stats[2] / values.target.stats[3];
+        }
     }
 
+    if(values.mossa.types[2]){
+        for (int i = 0; i < values.mossa.effects.size(); i++)
+            values.target.status.push_back(values.mossa.effects[i]);
+    }
 }
 
 
@@ -31,46 +43,60 @@ int battle(team team1, team team2, team total){
     int outcome = 0;
     int currentturn = 0;
     unsigned int BattleTimer = 0;
-    //1     2       3
+    bool IsAlive[6] = {1, 1, 1, 1, 1, 1};
+    // 1     2       3
     //
-    //   4      5       6
-
-
+    //    4      5       6
 
     //set initial timers
-    for (int i = 0; i<team1.members.size(); i++){
-        team1.members[i].set_timer();
-    }
-    for (int i = 0; i<team2.members.size(); i++){
-        team1.members[i].set_timer();
+    for (int i = 0; i<total.members.size(); i++){
+        total.members[i].set_timer();
     }
 
 
+    // main cycle
     while(true){
         BattleTimer++;
 
     // update timers
-        for (int i = 0; i<team1.members.size(); i++){
-            team1.members[i].timer--;
+        for (int i = 0; i<total.members.size(); i++){
+            total.members[i].timer--;
         }
-        for (int i = 0; i<team2.members.size(); i++){
-            team1.members[i].timer--;
-        }
-
 
         // take turns
         for (int i = 0; i<team1.members.size(); i++){
             if (team1.members[i].timer == 0){
                 currentturn = i+1;
                 apply_move(team1.members[i].taketurn(total));
+                for (int j = 0; j < total.members.size(); j++){
+                    if (total.members[j].hp < 0){
+                        IsAlive[j] = 0;
+                        total.members[j].hp = 0;
+                    }
+                }
             }
         }
-    
-
-
+        for (int i = 0; i<team2.members.size(); i++){
+            if (team2.members[i].timer == 0){
+                currentturn = i+1;
+                apply_move(team2.members[i].AItaketurn(total));
+                for (int j = 0; j < total.members.size(); j++){
+                    if (total.members[j].hp < 0){
+                        IsAlive[j] = 0;
+                        total.members[j].hp = 0;
+                    }
+                }
+            }
+        }
+        
+        if (IsAlive[0] == 0 && IsAlive[1] == 0 && IsAlive[2] == 0){
+            outcome = 2;
+            break;
+        }
+        if (IsAlive[3] == 0 && IsAlive[4] == 0 && IsAlive[5] == 0){
+            outcome = 1;
+            break;
     }
-
-
 
     //return 1 if team1 wins, 2 if team2 wins
     return outcome;
